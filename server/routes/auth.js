0e2c7f4b5c547b40cd783889c584b20cf51b2e72
sankalp <sankalp25103@gmail.com>
2023-12-23 11:24:00 +0530

    server done, logout done

---
+++ server/routes/auth.js
@@ -0,0 +1,7 @@
+const express=require('express')
+const router=express.Router()
+const {signup, login}=require('./../controllers/auth.js')
+router.post('/signup', signup)
+router.post('/signin', login)
+
+module.exports=router;
